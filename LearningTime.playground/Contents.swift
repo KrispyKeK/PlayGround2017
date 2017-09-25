bd7b00be7ed351cf4eb5df01fd67e2f8b8fd655c
JJ <infernoj2581@gmail.com>
2017-09-25 08:36:14 -0600

    Added more indepth stuff

--- LearningTime.playground/Contents.swift
+++ LearningTime.playground/Contents.swift
@@ -2,4 +2,43 @@
 
 import UIKit
 
-var str = "Hello, playground"
+//creates a variable called ramen which is declared as a string
+var ramen:String
+ramen = "Yum yum"
+print(ramen)
+
+//altering the value within ramen to "gross"
+ramen = "Gross"
+print(ramen + " ramen is gross")
+
+//creats a variable and assigning it a value to determine its value type
+var soup = "ramen"
+print(soup)
+//reassign the value of ramen to 7
+ramen = "7"
+
+//creating a double because why not
+var number:double_t = 6.9
+print(number)
+
+//creating an age variable that stores an integer value
+var myAge: Int = 5
+print(myAge)
+
+//using math operators to alter the value of myAge variable
+myAge = myAge * 3
+var FloatNum = 3.1415 * 56
+//can the plus equals to reassign the value by adding to the value
+FloatNum += 3.14
+
+//creating a variable that is a boolean(true or false)
+var isTrue:Bool
+isTrue = false
+isTrue = true
+//can print the boolean value
+print(isTrue)
+
+//creating an constant string value that will hold the same value forever
+let myName: String
+myName = "Juan Delacruz"
+print(myName)
