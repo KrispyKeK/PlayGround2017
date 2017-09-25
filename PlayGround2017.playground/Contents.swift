ca8e473c772b4a96b8fd6867acd1c9993975c39f
JJ <infernoj2581@gmail.com>
2017-09-25 08:08:41 -0600

    Added another file

---
+++ PlayGround2017.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var myName = "Juan Delacruz"
+print(myName)
+print("Test")
+
+var sample = "derpy"
+print(sample)
+sample = "herp a derp"
+print(sample)
+
+let test = 9
+print(test)
+
+
